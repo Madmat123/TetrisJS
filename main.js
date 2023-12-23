fb491799934bf1c7bf30f05a9332664a45a05bce
Madmat123 <mmiedzinski@gmail.com>
2023-12-23 22:35:14 +0100

    working game

--- main.js
+++ main.js
@@ -1,7 +1,59 @@
 import './style.css'
 
+const TETROMINOS = [
+    // Tetromino I
+    [
+    //   [0, 0, 0, 0],
+        [1, 1, 1, 1]//,
+    //    [0, 0, 0, 0],
+    //    [0, 0, 0, 0]
+    ],
+    // Tetromino J
+    [
+        [1, 0, 0],
+        [1, 1, 1],
+    //    [0, 0, 0]
+    ],
+    // Tetromino L
+    [
+        [0, 0, 1],
+        [1, 1, 1],
+    //    [0, 0, 0]
+    ],
+    // Tetromino O
+    [
+        [1, 1],
+        [1, 1]
+    ],
+    // Tetromino S
+    [
+        [0, 1, 1],
+        [1, 1, 0],
+    //    [0, 0, 0]
+    ],
+    // Tetromino T
+    [
+        [0, 1, 0],
+        [1, 1, 1],
+    //    [0, 0, 0]
+    ],
+    // Tetromino Z
+    [
+        [1, 1, 0],
+        [0, 1, 1],
+    //    [0, 0, 0]
+    ]
+  ];
+// Generate array of tetrominos with all rotations
+const tetrominosWithAllRotations = generateAllRotations(TETROMINOS);
+
 let canvas = document.getElementById("canvas");
 let ctx =  canvas.getContext("2d");
+
+let isGamePaused = false;
+
+ctx.strokeStyle = '#000'; // Set the stroke color
+
 console.log(ctx);
 
 canvas.width = 300;
@@ -9,116 +61,306 @@ canvas.height = 600;
 
 const width = canvas.width;
 const height = canvas.height;
-
 const columns = 10;
 const rows = 20;
 const squareSize = 30;
 const boardWidth = 10; // Width of the Tetris board in blocks
 const boardHeight = 20; // Height of the Tetris board in blocks
-const grid = [];
 
-ctx.strokeStyle = '#000'; // Set the stroke color
+let canvasPreview = document.getElementById("preview");
+let ctxPreview =  canvasPreview.getContext("2d");
+canvasPreview.width = 120;
+canvasPreview.height = 120;
 
-// Generate top-left corner coordinates of squares
-for (let i = 0; i < columns; i++) {
-  grid.push([]);
-    for (let j = 0; j < rows; j++) {
-        const x = (i * width) / columns;
-        const y = (j * height) / rows;
-        grid[i].push({ x: x, y: y }); // Push coordinates of top-left corner of squares
 
-        // Draw squares
-        ctx.strokeRect(x, y, squareSize, squareSize);
-    }
+let grid = Array.from({ length: boardHeight }, () => Array(boardWidth).fill(0));
+
+let boundingBox = []
+let score = 0;
+let rotation = 0;
+let tetrominoX = 4;
+let tetrominoY = 0;
+let currentX = tetrominoX;
+let currentY = tetrominoY;
+let speed = 700;
+
+const scoreDisplay = document.getElementById("score");
+console.log(`scoreDisplay: ${scoreDisplay}`);
+scoreDisplay.innerHTML = "Score: " + score;
+
+const pauseButton = document.getElementById("pause");
+
+let currentTetromino = getRandomTetromino(tetrominosWithAllRotations);
+let nextTetromino = getRandomTetromino(tetrominosWithAllRotations); 
+
+function ActivateNewTetromino(){
+    rotation = 0;
+    tetrominoX = 4;
+    tetrominoY = 0;
+    currentX = tetrominoX;
+    currentY = tetrominoY;
+    speed = 500;
 }
 
 console.log(grid); // Outputs the grid array containing top-left corner coordinates of squares
 
-const tetrominos = [
-  // Tetromino I
-  [
-      [0, 0, 0, 0],
-      [1, 1, 1, 1],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0]
-  ],
-  // Tetromino J
-  [
-      [1, 0, 0],
-      [1, 1, 1],
-      [0, 0, 0]
-  ],
-  // Tetromino L
-  [
-      [0, 0, 1],
-      [1, 1, 1],
-      [0, 0, 0]
-  ],
-  // Tetromino O
-  [
-      [1, 1],
-      [1, 1]
-  ],
-  // Tetromino S
-  [
-      [0, 1, 1],
-      [1, 1, 0],
-      [0, 0, 0]
-  ],
-  // Tetromino T
-  [
-      [0, 1, 0],
-      [1, 1, 1],
-      [0, 0, 0]
-  ],
-  // Tetromino Z
-  [
-      [1, 1, 0],
-      [0, 1, 1],
-      [0, 0, 0]
-  ]
-];
-console.log(tetrominos);
+console.log(TETROMINOS);
+// let tetrominoX = 0;
+// let tetrominoY = 0;
 
-function getRandomTetromino() {
-  const randomIndex = Math.floor(Math.random() * tetrominos.length);
-  return tetrominos[randomIndex];
+function getRandomTetromino(tetrominos) {
+    const randomIndex = Math.floor(Math.random() * tetrominos.length);
+    return tetrominos[randomIndex];
+}
+function drawPreview (tetromino){
+    ctxPreview.clearRect(0, 0, canvasPreview.width, canvasPreview.height);
+    for (let row = 0; row < tetromino[0].length; row++) {
+        for (let col = 0; col < tetromino[0][row].length; col++) {
+            if (tetromino[0][row][col]) {
+                const x = col* squareSize;
+                const y = row * squareSize;
+                ctxPreview.fillStyle = 'red'; // Set color for Tetris block
+                ctxPreview.fillRect(x, y, squareSize, squareSize);
+                ctxPreview.strokeStyle = 'black'; // Set border color
+                ctxPreview.strokeRect(x, y, squareSize, squareSize);
+            }
+        }
+    }
 }
 
-function drawTetromino(tetromino, xOffset, yOffset) {
-  for (let row = 0; row < tetromino.length; row++) {
-      for (let col = 0; col < tetromino[row].length; col++) {
-          if (tetromino[row][col]) {
-              const x = (col + xOffset) * squareSize;
-              const y = (row + yOffset) * squareSize;
-              ctx.fillStyle = 'blue'; // Set color for Tetris block
-              ctx.fillRect(x, y, squareSize, squareSize);
-              ctx.strokeStyle = 'black'; // Set border color
-              ctx.strokeRect(x, y, squareSize, squareSize);
-          }
-      }
-  }
+function drawTetromino(tetromino, rotation, xOffset, yOffset) {
+    grid.forEach((vElement, yIndex)=>{
+        vElement.forEach((xElement, xIndex)=>{
+            if(xElement) {
+                //console.log(`hIndex 4: ${hIndex} vIndex 4: ${vIndex}`)
+                const xGrid = xIndex * squareSize;
+                const yGrid = yIndex * squareSize;
+                ctx.fillStyle = 'grey'; // Set color for Grid block
+                ctx.fillRect(xGrid, yGrid, squareSize, squareSize);
+                ctx.strokeStyle = 'black'; // Set border color
+                ctx.strokeRect(xGrid, yGrid, squareSize, squareSize);
+            }
+        })
+    });
+
+    for (let row = 0; row < tetromino[rotation].length; row++) {
+        for (let col = 0; col < tetromino[rotation][row].length; col++) {
+            if (tetromino[rotation][row][col]) {
+                const x = (col + xOffset) * squareSize;
+                const y = (row + yOffset) * squareSize;
+                ctx.fillStyle = 'red'; // Set color for Tetris block
+                ctx.fillRect(x, y, squareSize, squareSize);
+                ctx.strokeStyle = 'black'; // Set border color
+                ctx.strokeRect(x, y, squareSize, squareSize);
+            }
+        }
+    }
+}
+function validateTetrominoPosition(tetromino, rotation, xOffset, yOffset){
+    let isPositionValid = true;
+    tetromino[rotation].forEach((row, rowIndex)=>{
+        row.forEach((cellValue, colIndex)=>{
+            if(yOffset+rowIndex < boardHeight && xOffset+colIndex <boardWidth){
+                if(cellValue && grid[yOffset+rowIndex][xOffset+colIndex]) {
+                    isPositionValid = false;
+                }
+            } else isPositionValid = false;
+        });
+    });           
+    return isPositionValid;
+}
+function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+function getBoundingBox(tetromino) {
+    let firstRow = -1;
+    let lastRow = -1;
+    let firstColumn = -1;
+    let lastColumn = -1;
+
+    // Find first row
+    for (let i = 0; i < tetromino.length; i++) {
+        if (tetromino[i].some(block => block === 1)) {
+            firstRow = i;
+            break;
+        }
+    }
+
+    // Find last row
+    for (let i = tetromino.length - 1; i >= 0; i--) {
+        if (tetromino[i].some(block => block === 1)) {
+            lastRow = i;
+            break;
+        }
+    }
+
+    // Find first column
+    for (let i = 0; i < tetromino[0].length; i++) {
+        if (tetromino.some(row => row[i] === 1)) {
+            firstColumn = i;
+            break;
+        }
+    }
+
+    // Find last column
+    for (let i = tetromino[0].length - 1; i >= 0; i--) {
+        if (tetromino.some(row => row[i] === 1)) {
+            lastColumn = i;
+            break;
+        }
+    }
+    return {
+        firstRow: firstRow,
+        lastRow: lastRow,
+        firstColumn: firstColumn,
+        lastColumn: lastColumn
+    };
 }
 
-// Function to animate tetromino falling with speed control
-function animateTetromino(tetromino, startX, startY, speed) {
-  let currentY = startY;
-
-  function animate() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      drawTetromino(tetromino, startX, currentY);
-      currentY++;
-
-      if (currentY <= boardHeight - tetromino.length) {
-          setTimeout(() => {
-              requestAnimationFrame(animate);
-          }, speed);
-      }
-  }
-
-  animate();
+function SetNewSpeed(s) {
+    speed = s;
+    drawTetromino(currentTetromino, rotation, tetrominoX, tetrominoY);
 }
 
-// Get a random tetromino and start the falling animation from the top-left corner with a speed of 500ms (adjust as needed)
-const randomTetromino = getRandomTetromino();
-animateTetromino(randomTetromino, 4, 0, 500); // Passing speed as 500 milliseconds
+function addTetrominoToGrid(){
+    const newGrid = [];
+    let scoreMultiplier = 0;
+    currentTetromino[rotation].forEach((vElement, vIndex)=>{
+        vElement.forEach((hElement, hIndex)=>{
+            if(hElement) {
+                grid[tetrominoY+vIndex-1][tetrominoX+hIndex] = 1;
+            }
+        })
+    });
+    grid.forEach(row => {
+        if (row.every(cell => cell !== 0)) {
+            const newRow = Array(row.length).fill(0); // Create a new row with 0 values
+            newGrid.unshift(newRow); // Add the new row at index 0
+            scoreMultiplier = (1 + scoreMultiplier) * 2;
+        } else
+        newGrid.push(row); // Push the original row
+    });
+    score = score + 10 * scoreMultiplier;
+    grid = newGrid;
+    console.log(grid);
+}
+
+function moveTetromino(direction) {          
+    if (direction === 'left' && tetrominoX - boundingBox.firstColumn> 0 && validateTetrominoPosition(currentTetromino, rotation, tetrominoX-1, tetrominoY)) {
+        clearCanvas();
+        tetrominoX--;
+    } else if (direction === 'right' && tetrominoX < boardWidth-boundingBox.lastColumn - 1 && validateTetrominoPosition(currentTetromino, rotation, tetrominoX+1, tetrominoY)) {
+        clearCanvas();
+        tetrominoX++;
+    }
+    drawTetromino(currentTetromino, rotation, tetrominoX, tetrominoY);  
+}
+function getNextRotation(r) {
+        if(validateTetrominoPosition(currentTetromino, (r + 1) % currentTetromino.length, tetrominoX, tetrominoY)){
+        rotation = (r + 1) % currentTetromino.length;
+        clearCanvas();
+        drawTetromino(currentTetromino, rotation, tetrominoX, tetrominoY); 
+        boundingBox = getBoundingBox(currentTetromino[rotation]);
+    }
+}    
+
+function rotateTetromino(tetromino) {
+    const rotatedTetromino = tetromino[0].map((_, colIndex) =>
+        tetromino.map(row => row[colIndex]).reverse()
+    );
+    return rotatedTetromino;
+}
+
+// Function to generate all rotations for a single tetromino
+function generateRotations(tetromino, rotations = []) {
+    rotations.push(tetromino);
+    const rotated = rotateTetromino(tetromino);
+    if (
+        JSON.stringify(rotated) !== JSON.stringify(tetromino) &&
+        !rotations.some(rotate => JSON.stringify(rotate) === JSON.stringify(rotated))
+    ) {
+        generateRotations(rotated, rotations);
+    }
+    return rotations;
+}
+
+// Function to generate all rotations for an array of tetrominos
+function generateAllRotations(tetrominos) {
+    const tetrominosWithRotations = tetrominos.map(tetromino =>
+        generateRotations(tetromino)
+    );
+    return tetrominosWithRotations;
+}
+
+// Display the tetrominos with their rotations (for demonstration purposes)
+console.log(tetrominosWithAllRotations);
+
+// Keyboard event listeners
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'a' || event.key === 'A') {
+        moveTetromino('left');
+    } else if (event.key === 'd' || event.key === 'D') {
+        moveTetromino('right');
+    } else if (event.key === 's' || event.key === 'S') {
+        speed =50;    
+    } else if (event.key === 'w' || event.key === 'W') {
+        getNextRotation(rotation);
+    }
+});
+document.getElementById('start').addEventListener('click', startGame);
+document.getElementById('pause').addEventListener('click', setPause);
+
+// Animation loop with reduced speed
+function animate() {
+    if (!isGamePaused){
+        clearCanvas();
+        drawTetromino(currentTetromino, rotation, tetrominoX, tetrominoY);
+        let isPosValid = validateTetrominoPosition(currentTetromino, rotation, tetrominoX, tetrominoY);
+        if(isPosValid ){
+            tetrominoY++; 
+            // Adjust the delay (in milliseconds) to control the falling speed 
+            if (!isGamePaused) {
+                setTimeout(() => {
+                    requestAnimationFrame(animate);
+                }
+            , speed); // Change the delay time here (e.g., 500ms for slower speed)
+            } 
+        }
+        else {
+            console.log(`${currentTetromino[rotation]}, x: ${tetrominoX}, y: ${tetrominoY}`);
+            addTetrominoToGrid();
+            currentTetromino = nextTetromino;
+            nextTetromino = getRandomTetromino(tetrominosWithAllRotations);
+            drawPreview(nextTetromino);
+            ActivateNewTetromino();
+            score+=10;
+            scoreDisplay.innerHTML = "Score: " + score;
+            requestAnimationFrame(animate);
+        }
+    }   
+};
+// Initial start of animation
+function startGame(){
+    isGamePaused = false;
+    grid = Array.from({ length: boardHeight }, () => Array(boardWidth).fill(0));
+    score = 0;
+    currentTetromino = getRandomTetromino(tetrominosWithAllRotations);
+    nextTetromino = getRandomTetromino(tetrominosWithAllRotations); 
+    rotation = 0;
+    tetrominoX = 4;
+    tetrominoY = 0;
+    currentX = tetrominoX;
+    currentY = tetrominoY;
+    speed = 700;
+    console.log("Button pressed");
+    drawPreview(nextTetromino);
+    boundingBox = getBoundingBox(currentTetromino[rotation]);
+    console.log("Bounding Box for Tetromino J:", boundingBox.firstRow);
+    requestAnimationFrame(animate);
+}
+
+function setPause(){
+    isGamePaused = !isGamePaused;
+    pauseButton.innerHTML = isGamePaused? "Unpause" : "Pause";
+    isGamePaused? true:requestAnimationFrame(animate);
+}
